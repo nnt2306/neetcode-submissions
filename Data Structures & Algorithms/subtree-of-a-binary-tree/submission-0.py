84d0f2955e5e55484f11b6f7d14bf57fc8956872
nnt2306 <163756559+nnt2306@users.noreply.github.com>
2026-04-18 09:26:07 +0200

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:  
+    def isSameTree(self,a:Optional[TreeNode],b:Optional[TreeNode]):
+        if a is None and b is None:
+            return True
+        
+        if a is None or b is None:
+            return False
+        
+        if a.val != b.val:
+            return False
+        if not self.isSameTree(a.left,b.left):
+            return False
+        if not self.isSameTree(a.right,b.right):
+            return False
+        return True
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if root is None and subRoot is None:
+            return True
+        
+        if root is None or subRoot is None:
+            return False
+        
+
+        ## For or if any condition True this will return immediately, otherwise this will go 
+        ## sequentially
+
+        ## In this either the trees are indentical or subRoot is a subtree of left child
+        # or subRoot is subtree of right child, the main check is the isSameTree
+        # When we do recursively this will check is Same tree for root left and root right
+        # with subRoot, this is the main things    
+        return (
+        self.isSameTree(root, subRoot)
+        or self.isSubtree(root.left, subRoot)
+        or self.isSubtree(root.right, subRoot)
+    ) 
+
+
+        
